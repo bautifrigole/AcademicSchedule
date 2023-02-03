a545e1d1ffb830a644dc2cdd26b0148169b6f4ae
Bautista Frigolé <bautistafrigole@gmail.com>
2023-02-03 20:32:33 -0300

    Added screens with tabs

---
+++ Flutter project/lib/screens/home_screen.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget{
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  int currentPage = 0;
+  final pageController = PageController( initialPage: 0 );
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Academic Schedule"),
+        centerTitle: true,
+        elevation: 0,
+      ),
+
+      body: PageView(
+        controller: pageController,
+        physics: const NeverScrollableScrollPhysics(),
+        children: const [
+          CustomScreen(color: Colors.black12,),
+          CustomScreen(color: Colors.black54,),
+        ],
+      ),
+
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentPage,
+
+        onTap: (index) {
+          currentPage = index;
+          pageController.animateToPage(index,
+              duration: const Duration( milliseconds: 300),
+              curve: Curves.easeOut);
+          setState(() {});
+        },
+
+        backgroundColor: Colors.blueAccent,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white.withOpacity(0.5),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home"
+          ),
+
+          BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: "Settings"
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomScreen extends StatelessWidget {
+  const CustomScreen({super.key, required this.color});
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: color,
+      child: const Center(
+        child: Text("Custom screen"),
+      ),
+    );
+  }
+}
